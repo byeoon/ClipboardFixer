3503f28d5001e56c4ee64f46dc57f7bde561bbac
byron <47872200+byeoon@users.noreply.github.com>
2024-04-14 12:33:14 -0400

    must run as admin, for some reason it crashes some apps

--- Program.cs
+++ Program.cs
@@ -6,27 +6,16 @@ namespace ClipboardFixer
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Title = "ClipboardFixer";
             Console.WriteLine("Now fixing Clipboard...");
-
-            System.Diagnostics.Process dwm = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo dwmStart = new System.Diagnostics.ProcessStartInfo();
-            dwmStart.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            dwmStart.FileName = "cmd.exe";
-            dwmStart.Arguments = "taskkill /f /im dwm.exe";
-            dwm.StartInfo = dwmStart;
-            dwm.Start();
-
-            System.Diagnostics.Process rdp = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo rdpStart = new System.Diagnostics.ProcessStartInfo();
-            rdpStart.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            rdpStart.FileName = "cmd.exe";
-            rdpStart.Arguments = "taskkill /f /im rdpclip.exe";
-            rdp.StartInfo = rdpStart;
-            rdp.Start();
+            Process.Start("taskkill", "/f /im dwm.exe");
+            Process.Start("taskkill", "/f /im rdpclip.exe");
 
             Process.Start("dwm.exe");
             Process.Start("rdpclip.exe");
-            Console.WriteLine("Clipboard fixed!");
+
+            Console.WriteLine("Clipboard fixed! Press any key to exit.");
             Console.ReadLine();
 
         }
